5830424271ff272bf9943d8008cd959cd9288505
Achim <Achim.Winter@yahoo.de>
2018-05-06 20:24:26 +0200

    implemented exercise1

--- src/main/java/assignment6/exercise1/PersonServlet.java
+++ src/main/java/assignment6/exercise1/PersonServlet.java
@@ -1,6 +1,8 @@
 package assignment6.exercise1;
 
 import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,19 +12,42 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "ServletExample", urlPatterns = {"/test"})
 public class PersonServlet extends HttpServlet {
 
+    List<Person> persons = new LinkedList<>();
 
     @Override
     protected void doPost(final HttpServletRequest request, final HttpServletResponse response) {
 
-        System.out.println(request.getAttribute("name"));
+        String lastName = request.getParameter("lastName");
+        String firstName = request.getParameter("firstName");
+        String birthday = request.getParameter("birthday");
+
+        Person person = new Person(lastName, firstName, birthday);
+        persons.add(person);
+
     }
 
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
             throws IOException {
-        System.out.println(request.getParameter("name"));
-        response.getWriter().print("Hello World");
-    }
 
+        if (request.getQueryString() == null) {
+            for (Person person : persons) {
+                response.getWriter().print(person.toString());
+            }
+        } else {
+            Map<String, String[]> parameters = request.getParameterMap();
+            for (Map.Entry<String, String[]> parameter : parameters.entrySet()) {
+                if (parameter.getKey().equals("name")) {
+                    for (int i = 0; i < parameter.getValue().length; i++) {
+                        for (Person p : persons) {
+                            if (p.getLastName().equals(parameter.getValue()[i])) {
+                                response.getWriter().print(p.toString());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
