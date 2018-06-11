07787ee67c3d2cdcb9b534c7905315bf7e1e4cb7
Achim <Achim.Winter@yahoo.de>
2018-06-11 16:05:15 +0200

    test

---
+++ src/main/java/assignment9/testMain.java
@@ -0,0 +1,11 @@
+package assignment9;
+
+import com.owlike.genson.Genson;
+
+public class testMain {
+
+    public static void main(String... args) {
+
+        Genson genson = new Genson();
+    }
+}
