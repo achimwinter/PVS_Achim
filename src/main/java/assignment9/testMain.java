4a316ce4e060db1d7e695ae0c8bfec51a08b5381
Achim <Achim.Winter@yahoo.de>
2018-06-11 16:05:49 +0200

    test

--- src/main/java/assignment9/testMain.java
+++ src/main/java/assignment9/testMain.java
@@ -1,11 +1,15 @@
 package assignment9;
 
+import assignment8.data.Vote;
 import com.owlike.genson.Genson;
 
 public class testMain {
 
-    public static void main(String... args) {
+    public static void main(final String... args) {
 
-        Genson genson = new Genson();
+        final Genson genson = new Genson();
+        Vote vote = new Vote();
+
+        String test = "hallo";
     }
 }
